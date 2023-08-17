57462b0567d48ad38082ca5584c3be3e257f7e1e
Liam Miller <admin@phyxphysio.co.nz>
2023-08-17 15:46:58 +1000

    Add draw and play again functions

--- main.py
+++ main.py
@@ -13,40 +13,57 @@ def choose_symbol():
         player_symbol = input('Welcome to tic tac toe!\nType x or o to choose your symbol.')
         if player_symbol not in approved_symbols:
             print('Invalid symbol. Choose either x or o.')
+    print(gameboard)
 
     opponent_symbol = 'o' if player_symbol == 'x' else 'x'
     return player_symbol, opponent_symbol
 
-def play(gameboard):
-    print(gameboard)
+def play(gameboard, player_symbol,opponent_symbol):
     
+    #Opponent Moves
+    possible_positions = [char for char in gameboard if char.isnumeric()]
+    move = random.choice(possible_positions)
+    print(f'Your opponent is placing an {opponent_symbol} at position {move}...')
+    gameboard = gameboard.replace(move, opponent_symbol)
+    if check_winner(gameboard, player_symbol, opponent_symbol):
+        print(gameboard)
+        play_again()
+        return
+    if is_draw(gameboard):
+            print("It's a draw!")
+            play_again()
+            return
+
+    print(gameboard) 
+
     #Player Moves
     position = input('Type a number to place your symbol there.')
     if position in gameboard and position.isnumeric():
         print(f'Placing {player_symbol} at position {position}...')
         gameboard = gameboard.replace(str(position), player_symbol)
         print(gameboard)
-        if check_winner(gameboard):
-            return 
-
-        #Opponent Moves
-        possible_positions = [char for char in gameboard if char.isnumeric()]
-        move = random.choice(possible_positions)
-        print(f'Your opponent is placing an {opponent_symbol} at position {move}...')
-        gameboard = gameboard.replace(move, opponent_symbol)
-        if check_winner(gameboard):
-            print(gameboard)
-            return 
+        if check_winner(gameboard, player_symbol, opponent_symbol):
+            play_again()
+            return
+        
 
     else:
         print('That is not a valid position, try again!')
-        play(gameboard)
+        play(gameboard, player_symbol, opponent_symbol)
 
     #Start next round
-    play(gameboard)
+    play(gameboard, player_symbol, opponent_symbol)
 
 
-def check_winner(gameboard):
+    possible_positions = [char for char in gameboard if char.isnumeric()]
+    move = random.choice(possible_positions)
+    print(f'Your opponent is placing an {opponent_symbol} at position {move}...')
+    gameboard = gameboard.replace(move, opponent_symbol)
+    if check_winner(gameboard):
+        print(gameboard)
+        return
+
+def check_winner(gameboard, player_symbol, opponent_symbol):
     for arr in winning_indices:
         if gameboard[arr[0]] in [player_symbol, opponent_symbol]:
             first_symbol = gameboard[arr[0]]
@@ -54,6 +71,21 @@ def check_winner(gameboard):
                 print(f'{first_symbol} wins!')
                 return True
 
+def is_draw(gameboard):
+    return not any(char.isnumeric() for char in gameboard)
 
-player_symbol, opponent_symbol = choose_symbol()
-play(gameboard)
+def play_again():
+    answer = None
+    while answer not in ['y','n']:
+        answer = input('Type y to play again and n to exit')
+    if answer == 'y':
+        start_game()
+    else:
+        return 
+
+def start_game():
+    player_symbol, opponent_symbol = choose_symbol()
+    play(gameboard,player_symbol=player_symbol,opponent_symbol=opponent_symbol  )
+
+if __name__ == '__main__':
+    start_game()
